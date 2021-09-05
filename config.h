30399aaae9f249dd7125c221232af8a7b826cff4
Tobias Lindholm <tobias.lindholm@antob.se>
2021-09-05 16:57:38 +0200

    Adjust for Polybar.

--- config.h
+++ config.h
@@ -4,11 +4,11 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"SFNSRegular:bold:pixelsize=14"
+	"SFNSRegular:bold:pixelsize=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*     				fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#282c34" },
 	[SchemeSel] = { "#191919", "#458588" },
 	[SchemeOut] = { "#000000", "#00ffff" },
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 21;
+static unsigned int lineheight = 40;
 static unsigned int min_lineheight = 8;
 /*
  * Characters not considered part of a word while deleting words
